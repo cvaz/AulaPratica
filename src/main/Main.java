1d00861dba210b54fceba2c2dae91bf5b8169c3c
cvaz <cvaz@cc.isel.ipl.pt>
2021-03-12 12:56:11 +0000

    Changing method

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,17 @@
 package main;
 
 public class Main {
-
-    public static void binarySearch(int[] array) {
-	// write your code here
+    public static int binarySearch(int[] array, int l, int r, int element){
+        int mid;
+        while(l<=r){
+            mid=l+(r-l)/2;
+            if(array[mid]==element) return mid;
+            else
+            if(array[mid]<element) l=mid+1;
+            else r=mid-1;
+        }
+        return -1;
     }
+
+
 }
--- test/BinarySearchTestClass.java
+++ test/BinarySearchTestClass.java
@@ -1,20 +1,18 @@
 import org.junit.jupiter.api.Test;
 import static main.Main.binarySearch;
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BinarySearchTestClass {
 
     @Test
     public void test_emptyArray(){
         int[] array=new int[0];
-        binarySearch(array);
-        assertArrayEquals(new int[0],array);
+        assertEquals(-1,binarySearch(array,0,-1,1));
     }
 
     @Test
     public void test_singletonArray(){
         int[] array= {1};
-        binarySearch(array);
-        assertArrayEquals(new int[]{1},array);
+        assertEquals(0,binarySearch(array,0,0,1));
     }
 }
